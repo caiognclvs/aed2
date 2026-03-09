772e7bf736a0846c8ebbafbf38e7068a1e585685
caio_gnclvs <caiognclvs@proton.me>
2026-03-09 05:27:40 -0300

    First commit

---
+++ revisao_alocacao_dinamica.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    double nota;
+    double frequencia;
+} Aluno;
+
+int main() {
+
+    int i, aprovados, reprovados_ambos, reprovados_nota, reprovados_frequencia;
+    double soma;
+    i = aprovados = reprovados_ambos = reprovados_nota = reprovados_frequencia = soma = 0;
+    
+    Aluno* alunos = calloc(30, sizeof(Aluno));
+
+    for (i = 0; i < 30; i++) {
+        scanf("%lf %lf\n", &alunos[i].nota, &alunos[i].frequencia);
+        soma += alunos[i].nota;
+
+        if (alunos[i].nota < 6.0 && alunos[i].frequencia < 75) reprovados_ambos++;
+        else if (alunos[i].nota < 6.0) reprovados_nota++;
+        else if (alunos[i].frequencia < 75) reprovados_frequencia++;
+        else aprovados++;
+    }
+
+    printf("Media geral da turma: %.2lf\n", soma / 30.0);
+    printf("Alunos aprovados: %d\nAlunos reprovados por nota: %d\nAlunos reprovados por frequencia: %d\nAlunos reprovados por nota e frequencia: %d\n", aprovados, reprovados_nota, reprovados_frequencia, reprovados_ambos);
+    free(alunos);
+
+    return 0;
+}
